6fd1156b5a19733024dbbe575164fcde8e343bcc
USAMI Kenta <tadsan@zonu.me>
2026-02-26 00:42:18 +0900

    Improve types

--- src/DecodeIssue.php
+++ src/DecodeIssue.php
@@ -6,10 +6,25 @@ namespace zonuexe\BrokenJson;
 
 final readonly class DecodeIssue
 {
+    /**
+     * @param int<-1, max> $position
+     */
     public function __construct(
         public DecodeIssueType $type,
         public string $message,
         public int $position = -1,
     ) {
+    }
+
+    /**
+     * @phpstan-return array{type: string, message: string, position: int<-1, max>}
+     */
+    public function toArray(): array
+    {
+        return [
+            'type' => $this->type->value,
+            'message' => $this->message,
+            'position' => $this->position,
+        ];
     }
 }
--- src/RepairAction.php
+++ src/RepairAction.php
@@ -6,10 +6,25 @@ namespace zonuexe\BrokenJson;
 
 final readonly class RepairAction
 {
+    /**
+     * @param 0|positive-int $position
+     */
     public function __construct(
         public RepairActionType $type,
         public int $position,
         public string $detail,
     ) {
+    }
+
+    /**
+     * @phpstan-return array{type: string, position: 0|positive-int, detail: string}
+     */
+    public function toArray(): array
+    {
+        return [
+            'type' => $this->type->value,
+            'position' => $this->position,
+            'detail' => $this->detail,
+        ];
     }
 }
